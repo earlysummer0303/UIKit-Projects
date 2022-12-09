d3d4d0c89bbb41777ebbaeb14e10570ea38462ed
earlysummer0303 <jw9914@naver.com>
2022-12-10 00:04:24 +0900

    PieChart 예제 구현 완료

---
+++ PomodoroTimerProject/PomodoroTimer/PomodoroTimer/TimerView.swift
@@ -0,0 +1,101 @@
+//
+//  TimerView.swift
+//  PomodoroTimer
+//
+//  Created by 황지우 on 2022/12/09.
+//
+
+import UIKit
+
+class TimerView: UIView {
+    
+    private let pieBezierPath = UIBezierPath()
+    
+    private let maskLayer = CAShapeLayer()
+    private let maskBezierPath = UIBezierPath()
+    
+    var colors: [CGColor]? {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    var values: [CGFloat]? {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+    override func setNeedsDisplay() {
+        super.setNeedsDisplay()
+        definePieChatLayer()
+    }
+    
+    
+    private func definePieChatLayer() {
+        // 주어진 각도 값으로 파이차트를 그리는 부분
+        guard let values = values else { return }
+        guard let colors = colors else { return }
+        
+        let center = CGPoint(x: bounds.midX, y: bounds.midY)
+        
+        var sAngle: CGFloat = 0
+        var eAngle: CGFloat = 0
+        
+        for i in 0..<values.count {
+            sAngle = eAngle
+            eAngle = sAngle + values[i]
+            
+            pieBezierPath.removeAllPoints()
+            pieBezierPath.move(to: center)
+            pieBezierPath.addArc(withCenter: center, radius: 150, startAngle: sAngle * 2 * CGFloat.pi, endAngle: eAngle * 2 * CGFloat.pi, clockwise: true)
+            pieBezierPath.close()
+            
+            let pieLayer = CAShapeLayer()
+            pieLayer.path = pieBezierPath.cgPath
+            pieLayer.lineWidth = 5
+            pieLayer.strokeEnd = 1
+            pieLayer.fillColor = colors[i]
+            pieLayer.strokeColor = UIColor.white.cgColor
+            
+            layer.addSublayer(pieLayer)
+        }
+        maskBezierPath.removeAllPoints()
+        maskBezierPath.addArc(withCenter: center, radius: 75, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+                
+        maskLayer.path = maskBezierPath.cgPath
+        maskLayer.lineWidth = 150
+        maskLayer.strokeEnd = 0
+        maskLayer.fillColor = UIColor.clear.cgColor
+        maskLayer.strokeColor = UIColor.black.cgColor
+            
+        layer.mask = maskLayer
+        
+    }
+    
+    func pieAnimate(duration: CFTimeInterval) {
+        maskLayer.strokeEnd = 0
+        maskLayer.removeAnimation(forKey: "maskAnimation")
+        
+        let pieAnimation = CABasicAnimation()
+        pieAnimation.keyPath = "strokeEnd"
+        pieAnimation.duration = duration
+        pieAnimation.toValue = 1
+        pieAnimation.fillMode = .forwards
+        pieAnimation.isRemovedOnCompletion = false
+        maskLayer.add(pieAnimation, forKey: "maskAnimation")
+    }
+    
+    
+}
--- PomodoroTimerProject/PomodoroTimer/PomodoroTimer/ViewController.swift
+++ PomodoroTimerProject/PomodoroTimer/PomodoroTimer/ViewController.swift
@@ -11,11 +11,14 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var timeCheckLabel: UILabel!
     
+    @IBOutlet weak var timerView: TimerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.timeCheckLabel.text = "60 Mins"
-        
+        timerView.colors = [UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor]
+        timerView.values = [0.25, 0.45, 0.10, 0.20]
+        timerView.pieAnimate(duration: 10.0)
     }
     
     @IBAction func resetButtonTabbed(_ sender: UIButton) {
